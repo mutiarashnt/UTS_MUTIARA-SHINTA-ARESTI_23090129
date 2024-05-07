b4f17542da29165407d63ef4f8bd7c4a0f2ab177
Mutiara Shinta <145634085+mutiarashnt@users.noreply.github.com>
2024-05-07 17:08:20 +0700

    Add files via upload

--- soal_2.py
+++ soal_2.py
@@ -1,9 +1,12 @@
 tahun = int(input('Masukkan Tahun : '))
 
-def tahun_kabisat(tahun)
-if tahun % 400 == 0:
-    return True
-elif tahun % 4 == 0 and tahun % 100 != 0:
-    return False
+def tahun_kabisat(tahun):
+    if tahun % 400 == 0:
+        return True
+    elif tahun % 4 == 0 and tahun % 100 != 0:
+        return False
+    else:
+        print('Bukan Tahun Kabisat')
+
 
 print('Tahun ', tahun, 'merupakan TAHUN KABISAT')
